da58e37a01a20905100e6d802d7b33795ceb758f
zodiacbalance <zodiacweight@gmail.com>
2017-10-11 14:22:33 +0300

    requests handled

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,15 +1,19 @@
-function sendPost(method){
-
+function send(method, form){
+    const url = "http://rest-api:8888/index.php",
+          mthd = method.toLowerCase();
+    console.log("method: ", mthd, "form: ", form);
+    // return false;
+    $.ajax(url, {
+            method: mthd
+        })
+        .done((response) => {
+            console.log('response=>', response);
+        })
+        .fail(() => {
+            console.log('Something went wrong');
+        })
+        .always(() => {
+            console.log('url=>', url);
+        });
 }
 
-function sendPut(method) {
-
-}
-
-function sendDelete(method){
-
-}
-
-function sendGet(method){
-
-}
