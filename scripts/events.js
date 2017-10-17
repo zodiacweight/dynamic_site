0452ea62dc49119182511d07fff17e7bb170ac29
srgg6701 <srgg67@gmail.com>
2017-10-17 21:10:24 +0300

    Fixing, refactoring

--- scripts/events.js
+++
@@ -1,6 +0,0 @@
-$("#view").on("mouseenter", ">div", (event)=>{
-    var word = event.target.innerText; 
-    getData(showTranslation, word, checked);
-    /*console.log("dictionary == undefined is ", dictionary === undefined);*/
-});
-
--- scripts/functions.js
+++ scripts/functions.js
@@ -1,5 +1,16 @@
-intro();
+$(function(){
+    /** 
+     * Проверяет символы в текстовом поле и, когда нужно, вызывает функцию createList
+    */
+    $("#word").on('input keyup', (e) => { // calling on jQuery object
+        //console.log("input: ", e.target.value);
+        (e.target.value.length > 2) ?
+            createList(e.target.value, checked)
+            : clearList();
+    });
+});
 
+const viewSelector = '#view';
 /** 
  * Get json file
  * @callback -- function to run inside after getting json
@@ -30,27 +41,6 @@ function initData() { // becomes getData after first calling
     }
 }
 
-
-
-/** 
- * Проверяет символы в текстовом поле и, когда нужно, вызывает функцию createList
- * ничего не возвращает
- * 
-*/
-function intro() {
-    // jQuery object
-    // $("#word")[0]; // HTML element!
-    var list = ""; const $input = $("#word");
-    $input.on('input keyup', (e) => { // calling on jQuery object
-        //console.log("input: ", e.target.value);
-        if (e.target.value.length > 2) {
-            createList(e.target.value, checked);
-        } else {
-            clearList();
-        }
-    });
-}
-
 /** Создает список слов, содержащих указанную подстроку и вставляет его в html.
  * параметры: 
  * словарь (из json-данных);
@@ -66,24 +56,33 @@ function makeWordsList(dictionary, substring, checked, currentWord) {
     let list = ""; // console.log('Keys=>', Object.keys(words));
     Object.keys(words).forEach((word) => { // console.log('check it=>', {word:word, wordData: words[word], words:words, substring:substring});
         if (word.indexOf(substring) !== -1) {
-            list += `<div class='word'>`+word+`</div>`;
-            /* const currentWord = word; words[word][0][0]; // console.log('Add word: ', {currentWord:currentWord});
-            if (currentWord) {
-                list += `<div class='word'>${currentWord}</div>`;
-            } else {
-                console.warn('Word is empty...');
-            } */
+            list += `<div class='word'>`+word+`
+        <section>`;
+            //console.log('word set=>', words[word]);
+            words[word][0].forEach((translatedWord, index) => {
+                const sentence = words[word][1][index] || '&nbsp;';
+                list += `
+                <div class='word'>
+                    <div class='wrapper'>
+                        <span>${translatedWord}</span>
+                        <div class="sentence">
+                            <div class='wrapper'>${sentence}</div>
+                        </div>
+                    </div>
+                </div>`;
+            });
+            list += `
+        </section>
+    </div>`;
         }
-    }); 
-    if (list) {
-        $("#view").html(list);
-    } else {
-        clearList();
-    }
+    });
+    (list) ?
+        $(viewSelector).html(list)
+        : learList();
 }
 
 function clearList(){
-    $("#view").html('');
+    $(viewSelector).html("");
 }
 
 /** Вызывает функцию getData и передает ей подстроку и переменную, означающую выбранный язык.
@@ -95,31 +94,4 @@ function clearList(){
 function createList(substring, checked, currentWord) {
     //console.log('createList', { substring: substring, checked: checked });
     getData(makeWordsList, substring, checked, currentWord);
-}
-
-/** Выводит список слов-переводов для конкретного слова.
- * Параметры: словарь из json-данных; 
- * массив русских слов, содержащийся в разделе словаря, соответствующего выбранному языку.
- */
-function showTranslation(dictionary, word, checked) {
-    var words = Object.keys(dictionary[languages[checked]][word][0]);
-    console.log("trans: ", trans);
-    for(var i=0, j=words.lenght; i<j; i++){
-       // trans.innerHTML+= "<div>"+word[i]+"</div>";
-       $("#translation").append(trans);
-        
-    } 
-    
-}
-
-
-/** Когда в строке 3 или больше букв:
- * 1. Пробег по словам по выбранному языку, поиск этого сочетания в каждом слове;
- * 2. В массив собрать все подходящие слова;
- * 3. сделать меню с этими словами;
- * 4. сделать так, чтобы при наведении мыши на слово появлялся список с предложениями
- * с этим словом.
-*/
-function lastState() {
-
 }
