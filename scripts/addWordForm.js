b231002932cdb00fbde4de809768b7fb8f962df1
zodiacbalance <zodiacweight@gmail.com>
2017-10-21 14:33:26 +0300

    before moving form and remaking the structure of json

--- scripts/addWordForm.js
+++ scripts/addWordForm.js
@@ -2,13 +2,27 @@ function addFields(){
     return "<div><input type=\"text\" placeholder=\"translation for word\"><textarea placeholder=\"sentence for word\"></textarea></div>";
 }
 
+$("body").html("<form id='addWordForm'>"
+                    +addFields()+
+                    "<input type=\"button\" value=\"добавить ячейку\" id=\"addWord\">"+
+                    "<input type='button' value='сохранить' id='save'>"+
+                "</form>");
+
 $("#addWord").on("click", (e) => {
     var addHTML=addFields();
     $("#addWordForm").append(addFields);
 });
 
 $("#save").on("click", (e) => {
-    $("#addWordForm div input").each(function(){
-        console.log('input.value=>', this.value);
+    $("#addWordForm div").each(function(){
+        var word = $(this).find("input"),
+        sentence = $(this).find("textarea");
+        word=word[0].value;
+        sentence = sentence[0].value;
+        //console.log('word: ', word, "sentence: ", sentence);
+        var checkedLanguage = getTargetLanguage();
+        console.log(checkedLanguage);
+       // localStorage.setItem(word, sentence);
     });
 });
+
