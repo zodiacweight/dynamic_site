a35bf81e9b9e0aaaac3cd80a89bfbd7e3475fcd3
Klevtsov <sergei.klevtsov@accenture.com>
2018-02-09 15:22:36 +0200

    Set redux and init

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,16 @@ const getData = initData(), // return function
 // store data in local dictionary
 getData();
 
+// ===============
+
+const { createStore } = Redux,
+    reducerSelectsValues = (state, action) => {
+        console.log('reducerSelectsValues reducer =>', {state:state, action:action});
+    },
+    storeSelectsValues = createStore(reducerSelectsValues);
+
+// ===============
+
 function output(fnc, args, color){
     color? console.groupCollapsed('%c'+fnc, 'color:'+color) : console.groupCollapsed(fnc);
     console.trace();
